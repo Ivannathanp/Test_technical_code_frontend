487ad2216e14136ba3c2a1e79b598838f3f72a83
Ivan Nathan Parmenas <ivanparmenas@gmail.com>
2022-07-16 16:11:53 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import react, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [number, setNumber] = useState();
+  const [data, setData] = useState([]);
+
+  async function GetPrime() {
+    console.log(number)
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api/prime",
+      data: {
+        number: number,
+      },
+    }).then((response) => {
+      console.log(response.data.data);
+      setData([response.data.data]);
+    });
+  }
+
+  async function GetOdd() {
+    console.log(number)
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api/ganjil",
+      data: {
+        number: number,
+      },
+    }).then((response) => {
+      console.log(response.data.data);
+      setData([response.data.data]);
+    });
+  }
+
+  // async function GetTriangle() {
+  //   axios({
+  //     method: "post",
+  //     url: "http://localhost:5000/api/triangle",
+  //     data: {
+  //       number: number,
+  //     },
+  //   }).then((response) => {
+  //     console.log(response);
+  //     setData([response]);
+  //   });
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type="text"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <div>
+          {/* <button type="button" onClick={GetTriangle}>
+            Generate Segitiga
+          </button> */}
+          <button type="button" onClick={GetOdd}>
+            Generate Bilangan Ganjil
+          </button>
+          <button type="button" onClick={GetPrime}>
+            Generate Bilangan Prima
+          </button>
+        </div>
+      </form>
+
+      <div>
+        <div>Result:</div>
+        {data.map((post, index) => {
+      console.log(post)
+   
+          return <div>{post}</div>;
+        })}
+      </div>
     </div>
   );
 }
